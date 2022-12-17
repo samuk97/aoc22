d66a8fc8b5432b0ef3e35c0437fd0f9b9927a2c1
Samuel Kemmler <samuel.kemmler@fau.de>
2022-12-17 21:15:44 +0100

    Add day 14 part 1

---
+++ 14/fallingSand.py
@@ -0,0 +1,78 @@
+import numpy as np
+import sys
+
+file = open("input.txt")
+
+lines = file.readlines()
+
+# Parsing
+minX = sys.maxsize
+maxX = 0
+maxY = 0
+for line in lines:
+    coordinates = line.split("->")
+    for coordinate in coordinates:
+        coordinate = coordinate.split(",")
+        minX = min(minX, int(coordinate[0]))
+        maxX = max(maxX, int(coordinate[0]))
+        maxY = max(maxY, int(coordinate[1]))
+
+cave = np.zeros((maxY + 1, maxX - minX + 1))
+
+for line in lines:
+    coordinates = line.split("->")
+    for i in range(len(coordinates) - 1):
+        point0 = coordinates[i].split(",")
+        point1 = coordinates[i + 1].split(",")
+        if int(point0[0]) < int(point1[0]) or int(point0[1]) < int(point1[1]):
+            cave[
+                int(point0[1]) : int(point1[1]) + 1,
+                int(point0[0]) - minX : int(point1[0]) - minX + 1,
+            ] = 1.0
+        else:
+            cave[
+                int(point1[1]) : int(point0[1]) + 1,
+                int(point1[0]) - minX : int(point0[0]) - minX + 1,
+            ] = 1.0
+
+# Part 1
+def produceSand(cave):
+    global minX
+    sandPos = [500 - minX, 0]
+    while True:
+        if (
+            cave[sandPos[1] + 1, sandPos[0]] != 1
+            and cave[sandPos[1] + 1, sandPos[0]] != 2
+        ):
+            sandPos[1] += 1
+        elif (
+            cave[sandPos[1] + 1, sandPos[0] - 1] != 1
+            and cave[sandPos[1] + 1, sandPos[0] - 1] != 2
+        ):
+            sandPos[0] -= 1
+            sandPos[1] += 1
+        elif (
+            cave[sandPos[1] + 1, sandPos[0] + 1] != 1
+            and cave[sandPos[1] + 1, sandPos[0] + 1] != 2
+        ):
+            sandPos[0] += 1
+            sandPos[1] += 1
+        else:
+            break
+
+    cave[sandPos[1], sandPos[0]] = 2
+
+
+counter = 0
+
+while True:
+    try:
+        produceSand(cave)
+        counter += 1
+    except:
+        break
+
+print(counter)
+
+# Part 2
+counter = 0
