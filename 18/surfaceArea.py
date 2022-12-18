e22b1204e70ceffda47cf89d0449c9489d3f561e
Samuel Kemmler <samuel.kemmler@fau.de>
2022-12-18 11:58:03 +0100

    Polish day 18 part 1

--- 18/surfaceArea.py
+++ 18/surfaceArea.py
@@ -22,18 +22,22 @@ for line in lines:
     line = line.split(",")
     droplet[int(line[2]), int(line[1]), int(line[0])] = 1.0
 
-# Part 1
-surfaceArea = 0
-directions = [[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]]
-for z in range(maxZ + 1):
-    for y in range(maxY + 1):
-        for x in range(maxX + 1):
-            if droplet[z, y, x] == 1.0:
-                for dir in directions:
-                    try:
-                        if droplet[z + dir[2], y + dir[1], x + dir[0]] == 0.0:
-                            surfaceArea += 1
-                    except:
+def computeSurfaceArea(part):
+    surfaceArea = 0
+    directions = [[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]]
+    for z in range(maxZ + 1):
+        for y in range(maxY + 1):
+            for x in range(maxX + 1):
+                if droplet[z, y, x] == 1.0:
+                    for dir in directions:
+                        try:
+                            if droplet[z + dir[2], y + dir[1], x + dir[0]] == 0.0:
+                                surfaceArea += 1
+                        except:
+                            surfaceArea += 1 
                         surfaceArea += 1
+                            surfaceArea += 1 
+    return surfaceArea
 
-print(surfaceArea)
+# Part 1
+print(computeSurfaceArea())
