340eb52dc54d5ec2e68c4cc41d62b54ac711c184
Samuel Kemmler <samuel.kemmler@fau.de>
2022-12-22 22:50:06 +0100

    Add day 13 part 2

--- 13/checkOrder.py
+++ 13/checkOrder.py
@@ -5,21 +5,29 @@ lines = [line for line in file.readlines() if line != "\n"]
 
 def compare(left, right):
     if type(left) == int and type(right) == int:
-        return left < right
+        if left < right:
+            return True
+        if left > right:
+            return False
+        return None
     if type(left) == int:
         left = [left]
     if type(right) == int:
         right = [right]
-    for i in range(len(left)):
+    for i in range(max(len(left), len(right))):
         if i >= len(right):  # right list runs out of items
             return False
-        elif left[i] == right[i]:
-            continue
-        else:
-            return compare(left[i], right[i])
-    return True  # left list runs out of items
+        if i >= len(left):  # left list runs out of items
+            return True
+        if compare(left[i], right[i]) == True:
+            return True
+        if compare(left[i], right[i]) == False:
+            return False
+        continue
+    return None  #
 
 
+# Part 1
 counter = 0
 for pair in range(int(len(lines) / 2)):
     # Sebastian told me about "eval()"
@@ -28,3 +36,27 @@ for pair in range(int(len(lines) / 2)):
         counter += pair + 1
 
 print(counter)
+
+# Part 2
+packets = []
+for line in lines:
+    if line != "\n":
+        packets.append(eval(line))
+
+divider0 = [[2]]
+divider1 = [[6]]
+packets.append(divider0)
+packets.append(divider1)
+
+changed = True
+while changed:
+    changed = False
+    for i in range(len(packets) - 1):
+        if not compare(packets[i], packets[i + 1]):
+            first = packets.pop(i)
+            second = packets.pop(i)
+            packets.insert(i, second)
+            packets.insert(i + 1, first)
+            changed = True
+
+print((packets.index(divider0) + 1) * (packets.index(divider1) + 1))
